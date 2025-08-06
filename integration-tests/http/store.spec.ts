7cd3793ea8005bfbb69f4dc78366104ff30c5144
SteelRazor47 <steelrazor47@gmail.com>
2025-08-06 20:54:12 +0200

    faster data creation, remove noise

--- integration-tests/http/store.spec.ts
+++ integration-tests/http/store.spec.ts
@@ -1,161 +1,82 @@
+import { MedusaContainer } from "@medusajs/medusa"
 import { medusaIntegrationTestRunner } from "@medusajs/test-utils"
-import { ApiKeyType, Modules, PUBLISHABLE_KEY_HEADER } from "@medusajs/utils"
+import { ApiKeyType, ContainerRegistrationKeys, Modules, PUBLISHABLE_KEY_HEADER } from "@medusajs/utils"
 import jwt from "jsonwebtoken"
 import { TOLGEE_MODULE } from "medusa-plugin-tolgee"
 
 jest.setTimeout(60000)
 
-const env = { MEDUSA_FF_MEDUSA_V2: true }
-const adminHeaders = { headers: {} }// "x-medusa-access-token": "test_token" } }
-
 medusaIntegrationTestRunner({
-  env,
-  testSuite: ({ dbConnection, getContainer, api }) => {
-    describe("Store: Shipping Option API", () => {
+  testSuite: ({ getContainer, api }) => {
+    describe("Shipping Option", () => {
       let salesChannel
       let region
-      let product
-      let stockLocation
-      let shippingProfile
-      let fulfillmentSet
-      let cart
       let shippingOption
-      let storeHeaders
+
+      let appContainer: MedusaContainer
+      const storeHeaders = { headers: {} }
+      const adminHeaders = { headers: {} }
 
       beforeAll(async () => {
-        const publishableKey = await getContainer().resolve(Modules.API_KEY).createApiKeys({
-          title: "test publishable key",
-          type: ApiKeyType.PUBLISHABLE,
-          created_by: "test",
-        })
-        storeHeaders = {
-          headers: {
-            [PUBLISHABLE_KEY_HEADER]: publishableKey.token,
-          },
-        }
+        appContainer = getContainer()
+      })
 
-        const container = getContainer()
+      beforeEach(async () => {
+        const authModuleService = appContainer.resolve("auth")
+        const userModuleService = appContainer.resolve("user")
 
-        const authModuleService = container.resolve("auth")
-        const userModuleService = container.resolve("user")
+        await Promise.all([
+          getPublishableKey(appContainer, storeHeaders),
+          getAdminToken(userModuleService, authModuleService, adminHeaders)
+        ])
 
-        const user = await userModuleService.createUsers({
-          email: "admin@medusa.js",
-
-        })
-        const authIdentity = await authModuleService.createAuthIdentities({
-          provider_identities: [
-            {
-              provider: "emailpass",
-              entity_id: "admin@medusa.js",
-              provider_metadata: {
-                password: "supersecret",
-              },
-            },
-          ],
-          app_metadata: {
-            user_id: user.id,
-          },
-        })
-
-        const token = jwt.sign(
-          {
-            actor_id: user.id,
-            actor_type: "user",
-            auth_identity_id: authIdentity.id,
-          },
-          "supersecret",
-          {
-            expiresIn: "1d",
-          }
-        )
-        adminHeaders.headers["authorization"] = `Bearer ${token}`
-
-        region = (
-          await api.post(
+        let shippingProfile
+        let stockLocation
+        [region, shippingProfile, salesChannel, stockLocation] = await Promise.all([
+          api.post(
             "/admin/regions",
             { name: "US", currency_code: "usd", countries: ["US"] },
             adminHeaders
-          )
-        ).data.region
-
-        shippingProfile = (
-          await api.post(
+          ).then(res => res.data.region),
+          api.post(
             `/admin/shipping-profiles`,
             { name: "Test", type: "default" },
             adminHeaders
-          )
-        ).data.shipping_profile
-
-        salesChannel = (
-          await api.post(
+          ).then(res => res.data.shipping_profile),
+          api.post(
             "/admin/sales-channels",
             { name: "first channel", description: "channel" },
             adminHeaders
-          )
-        ).data.sales_channel
-
-        product = (
-          await api.post(
-            "/admin/products",
-            {
-              title: "Test fixture",
-              options: [
-                { title: "size", values: ["large", "small"] },
-                { title: "color", values: ["green"] },
-              ],
-              shipping_profile_id: shippingProfile.id,
-              variants: [
-                {
-                  title: "Test variant",
-                  manage_inventory: false,
-                  prices: [
-                    {
-                      currency_code: "usd",
-                      amount: 100,
-                    },
-                    {
-                      currency_code: "dkk",
-                      amount: 100,
-                    },
-                  ],
-                  options: {
-                    size: "large",
-                    color: "green",
-                  },
-                },
-              ],
-            },
-            adminHeaders
-          )
-        ).data.product
-
-        stockLocation = (
-          await api.post(
+          ).then(res => res.data.sales_channel),
+          api.post(
             `/admin/stock-locations`,
             { name: "test location" },
             adminHeaders
-          )
-        ).data.stock_location
+          ).then(res => res.data.stock_location)
+        ])
 
-        await api.post(
-          `/admin/stock-locations/${stockLocation.id}/sales-channels`,
-          { add: [salesChannel.id] },
-          adminHeaders
-        )
-
-        const fulfillmentSets = (
-          await api.post(
+        const [fulfillmentSets] = await Promise.all([
+          api.post(
             `/admin/stock-locations/${stockLocation.id}/fulfillment-sets?fields=*fulfillment_sets`,
             {
               name: "Test",
               type: "test-type",
             },
             adminHeaders
+          ).then(res => res.data.stock_location.fulfillment_sets),
+          api.post(
+            `/admin/stock-locations/${stockLocation.id}/sales-channels`,
+            { add: [salesChannel.id] },
+            adminHeaders
+          ),
+          api.post(
+            `/admin/stock-locations/${stockLocation.id}/fulfillment-providers`,
+            { add: ["manual_manual"] },
+            adminHeaders
           )
-        ).data.stock_location.fulfillment_sets
+        ])
 
-        fulfillmentSet = (
+        const fulfillmentSet = (
           await api.post(
             `/admin/fulfillment-sets/${fulfillmentSets[0].id}/service-zones`,
             {
@@ -168,12 +89,6 @@ medusaIntegrationTestRunner({
             adminHeaders
           )
         ).data.fulfillment_set
-
-        await api.post(
-          `/admin/stock-locations/${stockLocation.id}/fulfillment-providers`,
-          { add: ["manual_manual"] },
-          adminHeaders
-        )
 
         shippingOption = (
           await api.post(
@@ -217,42 +132,47 @@ medusaIntegrationTestRunner({
         ).data.shipping_option
       })
 
-      afterAll(async () => {
-        const tolgeeService = getContainer().resolve(TOLGEE_MODULE)
-        await Promise.all([
-          tolgeeService.deleteTranslation(shippingOption.id),
-          tolgeeService.deleteTranslation(product.id),
-          tolgeeService.deleteTranslation(product.variants[0].id)
-        ])
+      afterEach(async () => {
+        const tolgeeService = appContainer.resolve(TOLGEE_MODULE)
+        await tolgeeService.deleteTranslation(shippingOption.id)
+      })
+
+      describe("Subscribers", () => {
+        test("shipping option has translations", async () => {
+          const query = appContainer.resolve(ContainerRegistrationKeys.QUERY)
+          const { data: shippingOptions } = await query.graph({
+            entity: "shipping_option",
+            fields: ["translations.*"],
+            filters: { id: shippingOption.id },
+          })
+
+          expect(shippingOptions).toHaveLength(1)
+          expect(shippingOptions[0]).toHaveProperty("translations")
+          expect(shippingOptions[0].translations).toMatchObject({
+            id: shippingOption.id,
+            it: { name: "Test shipping option" }
+          })
+        })
       })
 
       describe("GET /store/shipping-options?cart_id=", () => {
-        it("should get shipping options for a cart successfully", async () => {
-          cart = (
-            await api.post(
-              `/store/carts`,
-              {
-                region_id: region.id,
-                sales_channel_id: salesChannel.id,
-                currency_code: "usd",
-                email: "test@admin.com",
-                items: [
-                  {
-                    variant_id: product.variants[0].id,
-                    quantity: 1,
-                  },
-                ],
-              },
-              storeHeaders
-            )
-          ).data.cart
+        it("should get translated shipping options for a cart successfully", async () => {
+          const cart = (await api.post(
+            `/store/carts`,
+            {
+              region_id: region.id,
+              sales_channel_id: salesChannel.id,
+              currency_code: "usd",
+              email: "test@admin.com",
+              items: [],
+            },
+            storeHeaders
+          )).data.cart
 
-          const resp = await api.get(
+          const shippingOptions = (await api.get(
             `/store/shipping-options/tolgee?cart_id=${cart.id}`,
             storeHeaders
-          )
-
-          const shippingOptions = resp.data.shipping_options
+          )).data.shipping_options
 
           expect(shippingOptions).toHaveLength(1)
           expect(shippingOptions[0]).toHaveProperty("translations")
@@ -267,4 +187,44 @@ medusaIntegrationTestRunner({
     })
   },
 })
+
+async function getAdminToken(userModuleService, authModuleService, adminHeaders: { headers: {} }) {
+  const user = await userModuleService.createUsers({ email: "admin@medusa.js" })
+  const authIdentity = await authModuleService.createAuthIdentities({
+    provider_identities: [
+      {
+        provider: "emailpass",
+        entity_id: "admin@medusa.js",
+        provider_metadata: {
+          password: "supersecret",
+        },
+      },
+    ],
+    app_metadata: {
+      user_id: user.id,
+    },
+  })
+
+  const token = jwt.sign(
+    {
+      actor_id: user.id,
+      actor_type: "user",
+      auth_identity_id: authIdentity.id,
+    },
+    "supersecret",
+    {
+      expiresIn: "1d",
+    }
+  )
+  adminHeaders.headers["authorization"] = `Bearer ${token}`
+}
+
+async function getPublishableKey(appContainer: MedusaContainer, storeHeaders: { headers: {} }) {
+  const publishableKey = await appContainer.resolve(Modules.API_KEY).createApiKeys({
+    title: "test publishable key",
+    type: ApiKeyType.PUBLISHABLE,
+    created_by: "test",
+  })
+  storeHeaders.headers[PUBLISHABLE_KEY_HEADER] = publishableKey.token
+}
 
